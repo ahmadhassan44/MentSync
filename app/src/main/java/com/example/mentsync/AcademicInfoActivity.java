71696031101a0a23cc06c69f6683bc759b1af1f1
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2024-04-16 21:36:18 +0500

    added a dropdown listview to academic info activity
    The dropdown still need fixing

--- app/src/main/java/com/example/mentsync/AcademicInfoActivity.java
+++ app/src/main/java/com/example/mentsync/AcademicInfoActivity.java
@@ -3,14 +3,19 @@ package com.example.mentsync;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
 
 public class AcademicInfoActivity extends AppCompatActivity {
 
-    String[] disciplines={"BSCS","BSDS","BESE","BEEE"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_academic_info);
+
+        g
     }
 }
