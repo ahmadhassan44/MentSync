b0abf40cec551f6d440705fc0b088781e991525c
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2024-04-19 19:33:38 +0500

    Initial setting up
    I added the string reading functionality across EditTexts and made some preparatory steps for data processing

--- app/src/main/java/com/example/mentsync/LoginSignupActivity.java
+++ app/src/main/java/com/example/mentsync/LoginSignupActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginSignupActivity extends AppCompatActivity {
 
@@ -12,6 +14,22 @@ public class LoginSignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_signup);
+
+        String email=((EditText)findViewById(R.id.emailedit)).getText().toString();
+        String password=((EditText)findViewById(R.id.passwordedit)).getText().toString();
+        findViewById(R.id.loginbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!(email.isEmpty() || password.isEmpty()))
+                    startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                else if(password.isEmpty())
+                    ((EditText)findViewById(R.id.passwordedit)).setError("Can't be empty");
+                else if(email.isEmpty())
+                    ((EditText)findViewById(R.id.passwordedit)).setError("Can't be empty");
+                else if (!validate(email,password));
+                    Log.d("Ahmad","Invalid credentials");
+            }
+        });
         findViewById(R.id.newaccbtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -19,4 +37,12 @@ public class LoginSignupActivity extends AppCompatActivity {
             }
         });
     }
+        boolean validate(String email,String password)
+        {
+            return true;
+        }
+
+
+
+
 }
--- app/src/main/java/com/example/mentsync/PersonalInfoActivity.java
+++ app/src/main/java/com/example/mentsync/PersonalInfoActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class PersonalInfoActivity extends AppCompatActivity {
 
@@ -12,10 +14,23 @@ public class PersonalInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
+
         findViewById(R.id.next1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),AcademicInfoActivity.class));
+                String name=((EditText) findViewById(R.id.nameedit)).getText().toString();
+                String cms=((EditText) findViewById(R.id.cmsedit)).getText().toString();
+                String email=((EditText) findViewById(R.id.emailedit)).getText().toString();
+                if(!(email.isEmpty() || cms.isEmpty() || name.isEmpty() || cms.length()<6))
+                    startActivity(new Intent(getApplicationContext(),AcademicInfoActivity.class));
+                if(name.isEmpty())
+                    ((EditText) findViewById(R.id.nameedit)).setError("Can't be empty");
+                if(cms.isEmpty())
+                    ((EditText) findViewById(R.id.cmsedit)).setError("Can't be empty");
+                if(cms.length()<6)
+                    ((EditText) findViewById(R.id.cmsedit)).setError("Invalid CMS");
+                if(email.isEmpty())
+                    ((EditText) findViewById(R.id.emailedit)).setError("Can't be empty");
             }
         });
         findViewById(R.id.cancelbtn2).setOnClickListener(new View.OnClickListener() {
