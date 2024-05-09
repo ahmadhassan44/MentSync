d062ed1c21c34f2d7fb9d0a48db8b4c6749dc103
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2024-05-10 03:24:54 +0500

    More UI

--- app/src/main/java/com/example/mentsync/AfterLogin/AddFragmentChilds/QueryPostFragment.java
+++ app/src/main/java/com/example/mentsync/AfterLogin/AddFragmentChilds/QueryPostFragment.java
@@ -2,11 +2,16 @@ package com.example.mentsync.AfterLogin.AddFragmentChilds;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.mentsync.R;
 
@@ -17,50 +22,51 @@ import com.example.mentsync.R;
  */
 public class QueryPostFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    View queryfrag;
     public QueryPostFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QueryPostFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QueryPostFragment newInstance(String param1, String param2) {
-        QueryPostFragment fragment = new QueryPostFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_query_post, container, false);
+        queryfrag=inflater.inflate(R.layout.fragment_query_post, container, false);
+        return queryfrag;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        EditText queryedittext=queryfrag.findViewById(R.id.query);
+        queryedittext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(queryedittext.getText().length()>1)
+                {
+                    queryfrag.findViewById(R.id.button2).setVisibility(View.VISIBLE);
+                    queryfrag.findViewById(R.id.button3).setVisibility(View.VISIBLE);
+                }
+                if(queryedittext.getText().length()==0)
+                {
+                    queryfrag.findViewById(R.id.button2).setVisibility(View.INVISIBLE);
+                    queryfrag.findViewById(R.id.button3).setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
